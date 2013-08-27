39c28a28994e1d9a2e70f2b71fbb516528c85cdb
dragondjf <dragondjf@gmail.com>
2013-08-27 22:34:09 +0800

    修改主题获取路径的方式

--- md2html.py
+++ md2html.py
@@ -7,8 +7,8 @@ import codecs
 from Cheetah.Template import Template
 
 themes = {
-    'themeblack': [ os.sep.join([os.getcwd(), 'themecss', 'black', css]) for css in ['preview.css', 'style.css']],
-    'themewhite': [ os.sep.join([os.getcwd(), 'themecss', 'white', css]) for css in ['markdown.css']]
+    'themeblack': [os.sep.join([os.path.dirname(__file__), 'themecss', 'black', css]) for css in ['preview.css', 'style.css']],
+    'themewhite': [os.sep.join([os.path.dirname(__file__), 'themecss', 'white', css]) for css in ['markdown.css']]
 }
 
 
@@ -70,6 +70,7 @@ def md2html(mdfile, htmlfile, theme):
     # Write string html to disk
     with open(htmlfile, 'wb') as f:
         f.write(str(html))
+
 
 def main():
     mdfile = os.sep.join([os.getcwd(), 'demo.md'])
